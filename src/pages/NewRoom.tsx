563ccf768172a18efa00f4f287c3589cfb27d289
Marcelo Ferreira <mah.tf@hotmail.com>
2021-06-22 23:09:25 -0300

    Criado a tela de criar salas

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,18 +1,13 @@
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 
-export function Home() {
-  const history = useHistory();
-
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
-  }
+export function NewRoom() {
 
   return (
     <div id="page-auth">
@@ -25,20 +20,19 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Let me ask Logo" />
-          <button className="create-room" onClick={navigateToNewRoom}>
-            <img src={googleIconImg} alt="Google Logo" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator"> ou entre em uma sala </div>
+          <h2>Criar uma nova sala</h2>
           <form>
             <input
               type="text"
-              placeholder="digite o código da sala"
+              placeholder="digite o nome da sala"
             />
             <Button type="submit">
-              Entrar na sala
+              Criar na sala
             </Button>
           </form>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
